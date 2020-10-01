6d780fb5b536e95fdf76c7eb988e222010afab6c
Daniel <work.gabitov@gmail.com>
2020-10-01 19:18:19 +0300

    update

--- HW_04/main.kt
+++ HW_04/main.kt
@@ -1,16 +1,7 @@
-fun read_lines(): String {
-  var data: String = ""
-  var buffer: String? = readLine()
-  while (buffer != null) {
-    data += buffer
-    buffer = readLine()
-  }
-  return data
-}
+import java.io.File
+import java.lang.Math.max
 
-fun delete_spaces(data: String): String {
-  return data.filter { it != '\n' && it != '\t' && it != ' ' }
-}
+var best = - 1;
 
 fun is_over_string_edge(data: String, index: Int): Boolean{
   return index >= data.length
@@ -18,12 +9,12 @@ fun is_over_string_edge(data: String, index: Int): Boolean{
 
 fun parse_literal(data: String, state: Pair<Boolean, Int>) : Pair<Boolean, Int>{
   val index = state.second
-  if (!state.first or is_over_string_edge(data, index)){
+  if (!state.first or is_over_string_edge(data, index + 1)){
     return Pair(false, index)
   }
 
   val regex  = Regex("[a-z]")
-  val symbol = data[index].toString()
+  val symbol = data[index + 1].toString()
 
   if (regex.containsMatchIn(symbol)){
     return Pair(true, index + 1)
@@ -34,20 +25,20 @@ fun parse_literal(data: String, state: Pair<Boolean, Int>) : Pair<Boolean, Int>{
 
 fun parse_symbol(data: String, state: Pair<Boolean, Int>, symbol: Char) : Pair<Boolean, Int>{
   val index = state.second
-  if (!state.first or is_over_string_edge(data, index)){
+  if (!state.first or is_over_string_edge(data, index + 1)){
     return Pair(false, index)
   }
 
-  if (data[index] == symbol){
+  if (data[index + 1] == symbol){
     return Pair(true, index + 1)
   }
   return Pair(false, index)
 }
 
 fun parse_brackets_r(data: String, state: Pair<Boolean, Int>,
-                  parse_item: (data: String, state: Pair<Boolean,Int>) -> Pair<Boolean, Int>,
-                  parse_sep : (data: String, state: Pair<Boolean,Int>) -> Pair<Boolean, Int>)
-          : Pair<Boolean, Int> {
+                     parse_item: (data: String, state: Pair<Boolean,Int>) -> Pair<Boolean, Int>,
+                     parse_sep : (data: String, state: Pair<Boolean,Int>) -> Pair<Boolean, Int>)
+        : Pair<Boolean, Int> {
 
   if (!state.first){
     return Pair(false, state.second)
@@ -57,17 +48,28 @@ fun parse_brackets_r(data: String, state: Pair<Boolean, Int>,
   var sep_result     =  parse_sep(data, item_result)
   var bracets_result =  parse_brackets_r(data, sep_result, parse_item, parse_sep)
 
+  if (item_result.first){
+    best = max(best, item_result.second)
+  }
+  if (sep_result.first){
+    best = max(best, sep_result.second)
+  }
+  if (bracets_result.first){
+    best = max(best, bracets_result.second)
+  }
+
   if (bracets_result.first){
     return bracets_result
   }
-
+  if (sep_result.first){
+    return Pair(false, sep_result.second)
+  }
   item_result = parse_item(data, state)
 
   if (item_result.first){
     return item_result
   }
-
-  return Pair(false, state.second)
+  return Pair(false, max(item_result.second, bracets_result.second))
 }
 
 fun parse_p(data: String, state: Pair<Boolean, Int>): Pair<Boolean, Int>{
@@ -81,8 +83,20 @@ fun parse_p(data: String, state: Pair<Boolean, Int>): Pair<Boolean, Int>{
   val e_result = parse_e(data, open_bracket_result)
   val close_bracket_result = parse_symbol(data, e_result, ')')
 
+  if (open_bracket_result.first){
+    best = max(best, open_bracket_result.second)
+  }
+
+  if (e_result.first){
+    best = max(best, e_result.second)
+  }
+
+  if (close_bracket_result.first){
+    best = max(best, close_bracket_result.second)
+  }
+
   if (!close_bracket_result.first){
-    return Pair(false, state.second)
+    return Pair(false, max(literal_result.second, close_bracket_result.second))
   }
 
   return close_bracket_result
@@ -92,11 +106,11 @@ fun parse_p(data: String, state: Pair<Boolean, Int>): Pair<Boolean, Int>{
 
 fun parse_and_operator(data: String, state: Pair<Boolean, Int>): Pair<Boolean, Int>{
   val index = state.second
-  if (!state.first or is_over_string_edge(data, index)){
+  if (!state.first or is_over_string_edge(data, index + 1)){
     return Pair(false, index)
   }
 
-  if (data[index] == ','){
+  if (data[index + 1] == ','){
     return Pair(true, index + 1)
   }
   return Pair(false, index)
@@ -104,11 +118,11 @@ fun parse_and_operator(data: String, state: Pair<Boolean, Int>): Pair<Boolean, I
 
 fun parse_or_operator(data: String, state: Pair<Boolean, Int>): Pair<Boolean, Int>{
   val index = state.second
-  if (!state.first or is_over_string_edge(data, index)){
+  if (!state.first or is_over_string_edge(data, index + 1)){
     return Pair(false, index)
   }
 
-  if (data[index] == ';'){
+  if (data[index + 1] == ';'){
     return Pair(true, index + 1)
   }
   return Pair(false, index)
@@ -120,7 +134,7 @@ fun parse_m(data: String, state: Pair<Boolean, Int>): Pair<Boolean, Int>{
   }
   val result = parse_brackets_r(data, state, ::parse_p, ::parse_and_operator)
   if (!result.first){
-    return Pair(false, state.second)
+    return Pair(false, result.second)
   }
   return result
 }
@@ -131,7 +145,7 @@ fun parse_e(data: String, state: Pair<Boolean, Int>): Pair<Boolean, Int>{
   }
   val result = parse_brackets_r(data, state, ::parse_m, ::parse_or_operator)
   if (!result.first){
-    return Pair(false, state.second)
+    return Pair(false, result.second)
   }
   return result
 }
@@ -144,28 +158,76 @@ fun parse_main(data: String, state: Pair<Boolean, Int>) : Pair<Boolean, Int>{
   val colon_result = parse_symbol(data, lit_result, ':')
   val hyphen_result    = parse_symbol(data, colon_result, '-')
   val e_result     = parse_e(data, hyphen_result)
-  var dot_result   = parse_symbol(data, e_result, '.')
+  var dot_result_1   = parse_symbol(data, e_result, '.')
 
-  if (dot_result.first){
-    return dot_result
+  if (dot_result_1.first){
+    return dot_result_1
   }
 
-  dot_result = parse_symbol(data, lit_result, '.')
-  if (dot_result.first){
-    return dot_result
+  val dot_result_2 = parse_symbol(data, lit_result, '.')
+  if (dot_result_2.first){
+    return dot_result_2
   }
-  return Pair(false, state.second)
+  return Pair(false, max(dot_result_1.second, dot_result_2.second))
 }
 
-fun parse(data: String): Boolean{
-  val result = parse_main(data, Pair(true, 0))
-  return result.first and (result.second == data.length)
+fun parse(data: String): Pair<Boolean, Int>{
+  val result = parse_main(data, Pair(true, -1))
+  return Pair(result.first and (result.second == data.length - 1), max(result.second, best))
 }
+
 
 fun main() {
-  var data = delete_spaces(read_lines())
-  when (parse(data)){
-    true  -> println("YES")
-    false -> println("NO")
+  val file_data = File("test.txt").readText()
+  var data = file_data
+  var array = arrayOf<String>()
+  while (data.length != 0){
+    if (data.indexOf('.') == -1){
+      array += data
+      break;
+    }
+    array += data.subSequence(0, data.indexOf('.') + 1).toString()
+    data = data.subSequence(data.indexOf('.') + 1, data.length).toString()
   }
+
+  var error_symbol = -2;
+  var tmp = 0
+  for (word in array){
+    val parse_result = parse(word.filter { it != '\n' && it != ' '})
+    if (!parse_result.first){
+      error_symbol = parse_result.second
+      break;
+    }
+    best = -1
+    tmp += word.length
+  }
+  if (error_symbol == -2){
+    println("Everything is correct!")
+    return
+  }
+
+  var i = 0
+  var colon = 0
+  var line: Int
+  if (tmp == 0){
+    line = 1
+  } else{
+    line = 1 + file_data.substring(0, tmp).filter { it == '\n' }.length
+  }
+  data = file_data.substring(tmp, file_data.length)
+  for (symbol in data){
+    if (i == error_symbol + 1){
+      break;
+    }
+    if (symbol == '\n'){
+      line++
+      colon = 0
+    } else if (Regex("[a-z]|-|:|;|,|(|)").containsMatchIn(symbol.toString())){
+      i++;
+      colon++
+    } else {
+      colon++
+    }
+  }
+  println("Syntax error: line: $line, colon: $colon.")
 }
